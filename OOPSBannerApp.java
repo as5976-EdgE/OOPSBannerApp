81a01f4de050da9e502cd74717c303ca9d5feb79
as5976-EdgE <as5976@srmist.edu.in>
2026-02-17 10:21:17 +0530

    UC2

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        // Printing OOPS using stars
+        System.out.println(" *****  " + " *****  " + " *****  " + " ***** ");
+        System.out.println("**   ** " + "**   ** " + "**   ** " + "**     ");
+        System.out.println("**   ** " + "**   ** " + "**   ** " + "**     ");
+        System.out.println("**   ** " + "**   ** " + "*****   " + " ***** ");
+        System.out.println("**   ** " + "**   ** " + "**      " + "     **");
+        System.out.println("**   ** " + "**   ** " + "**      " + "**   **");
+        System.out.println(" *****  " + " *****  " + "**      " + " ***** ");
+
+    }
+}
+
